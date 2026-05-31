c1f603a41b6d0c8ae34533e7e235de46520599a7
Anna Kasianenko <annakasianenko2005@gmail.com>
2026-05-31 23:47:51 +0300

    fix3

--- snippets/books_statistics.php
+++ snippets/books_statistics.php
@@ -7,32 +7,48 @@ $pass = getenv('DB_PASSWORD');
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
-    $conn = new PDO($dsn, $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $row1 = $conn->query("SELECT COUNT(*) AS BooksNumber FROM books")->fetch(PDO::FETCH_ASSOC);
-    $row2 = $conn->query("SELECT COUNT(*) AS BooksNumber FROM books WHERE Status='в наявності'")->fetch(PDO::FETCH_ASSOC);
-    $row3 = $conn->query("SELECT COUNT(*) AS BooksNumber FROM books WHERE Status='на руках'")->fetch(PDO::FETCH_ASSOC);
-    $row4 = $conn->query("SELECT AVG(Price) AS BooksPrice FROM books")->fetch(PDO::FETCH_ASSOC);
+    $conn = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_TIMEOUT => 7,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
+    $row1 = $conn->query("SELECT COUNT(*) AS BooksNumber FROM books")
+                 ->fetch(PDO::FETCH_ASSOC);
+
+    $row2 = $conn->query("SELECT COUNT(*) AS BooksNumber FROM books WHERE Availability='в наявності'")
+                 ->fetch(PDO::FETCH_ASSOC);
+
+    $row3 = $conn->query("SELECT COUNT(*) AS BooksNumber FROM books WHERE Availability='на руках'")
+                 ->fetch(PDO::FETCH_ASSOC);
+
+    $row4 = $conn->query("SELECT AVG(Price) AS BooksPrice FROM books")
+                 ->fetch(PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
-    echo "<div class='validation-msg'><h2 class='validation-text'>Помилка бази даних</h2></div>";
+    echo "<div class='validation-msg'>
+            <h2 class='validation-text'>Помилка бази даних</h2>
+          </div>";
     exit;
 }
 ?>
+
 <div class="col-lg-3 statistics books">
     <h2>Загальна кількість книг в системі</h2>
-    <p class="number"><?php echo $row1['BooksNumber']; ?></p>
+    <p class="number"><?= $row1['BooksNumber'] ?? 0 ?></p>
 </div>
+
 <div class="col-lg-3 statistics books">
     <h2>Кількість книг в наявності</h2>
-    <p class="number"><?php echo $row2['BooksNumber']; ?></p>
+    <p class="number"><?= $row2['BooksNumber'] ?? 0 ?></p>
 </div>
+
 <div class="col-lg-3 statistics books">
     <h2>Кількість книг на руках</h2>
-    <p class="number"><?php echo $row3['BooksNumber']; ?></p>
+    <p class="number"><?= $row3['BooksNumber'] ?? 0 ?></p>
 </div>
+
 <div class="col-lg-3 statistics books">
     <h2>Середня вартість книги</h2>
-    <p class="number"><?php echo round($row4['BooksPrice']); ?> грн</p>
+    <p class="number"><?= round($row4['BooksPrice'] ?? 0) ?> грн</p>
 </div>
